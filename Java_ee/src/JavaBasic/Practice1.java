a339395bba3a866d5fab0682a7eb906e3a30ff52
876538875 <876538875@qq.com>
2021-12-05 22:51:31 +0800

    解除了一下网络编程，大胆往前走，理解概念是第一位

--- Java_ee/src/JavaBasic/Practice1.java
+++
@@ -1,4 +0,0 @@
-package JavaBasic;
-import java.util.Scanner;
-
-
--- Java_ee/src/JavaBasic/StringDemo.java
+++
@@ -1,9 +0,0 @@
-package JavaBasic;
-
-public class StringDemo {
-    public static void main(String[] args) {
-        String x = "abc";
-        boolean equals = x.equals("aca");
-        System.out.println(equals);
-    }
-}
